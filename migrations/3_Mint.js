9b08e71f87392f74f3ec53b93921b284028a2142
eduardoamdev <eduardoamdev@gmail.com>
2022-11-13 23:14:36 +0100

    migrate files cleaned

--- migrations/3_Mint.js
+++ migrations/3_Mint.js
@@ -1,7 +1,5 @@
 var TJoyMint = artifacts.require("./TJoyMint.sol");
 
-var wait = require("../scripts/helpers/wait");
-
 module.exports = async function (deployer) {
   await deployer.deploy(TJoyMint, 10000);
   console.log("Mint contract deployed");
